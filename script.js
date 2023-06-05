6e28e52142841935fb6020107b3c74a59511a61c
Salman Sadiq <118099469+salmansadiq1015@users.noreply.github.com>
2023-06-05 06:54:16 -0700

    Add files via upload
    next

---
+++ script.js
@@ -0,0 +1,62 @@
+//  Most Popular Movies
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
+// Search Movies
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+const movieBox = document.querySelector("#movie-box");
+
+const getMovies = async(api)=>{
+    const response = await fetch(api);
+    const data = await response.json();
+    // console.log(data);
+    showMovies(data.results);
+}
+
+const showMovies = (data)=>{
+    movieBox.innerHTML= "";
+    // empty the movie box
+    data.forEach(
+        (item)=>{
+            // console.log(item);
+           const box = document.createElement("div");
+           box.classList.add("box");
+           box.innerHTML = `
+             <img src="${IMGPATH + item.poster_path}" alt=""/>
+             <div class="overlay">
+                  <div class="title">
+                      <h2>${ item.original_title}</h2>
+                      <span> ${ item.vote_average} </span>
+                  </div>
+                  <h3>Overview:</h3>
+                  <p>
+                  ${ item.overview}
+                  </p>
+              </div>
+           `;
+           movieBox.appendChild(box);
+        }
+    );
+}
+
+document.querySelector("#search").addEventListener(
+    "keyup",
+    function(event){
+       if(event.target.value != "")
+       {
+        getMovies(SEARCHAPI + event.target.value);
+        // search movies
+       }else{
+        getMovies(APIURL)
+       }
+    }
+);
+
+// init call
+getMovies(APIURL)
